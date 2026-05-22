61a06d9c5d615c7252eb95ebbe8c3d0a0d410a0e
Adil Echajiai <levi7r@mc7r.local>
2026-05-23 00:16:04 +0100

    exam 05

--- exam05/mylvl2/bsq/bsq.c
+++ exam05/mylvl2/bsq/bsq.c
@@ -7,14 +7,14 @@ typedef struct {
 } square;
 
 int min3(int a, int b, int c) {
-    if (a <= b && a <= c)
+    if (a <= b && a <= c) 
         return a;
     if (b <= a && b <= c)
         return b;
     return c;
 }
 
-int ft_strlen(char *str) {
+int strlenn(char *str) {
     int i = 0;
     while (str[i])
         i++;
@@ -22,65 +22,63 @@ int ft_strlen(char *str) {
 }
 
 int main(int ac, char **av) {
-    FILE *ff = ac > 1 ? fopen(av[1], "r") : stdin;
-    if (!ff)
-        return fprintf(stdout, "file error\n"), 1;
+    FILE *fd = ac > 1 ? fopen(av[1], "r") : stdin;
+    if (!fd)
+        return (fprintf(stderr, "file error\n"), 1);
     int height = 0, width = 0;
     char empty = 0, obs = 0, full = 0;
-    if (fscanf(ff, "%d %c %c %c\n", &height, &empty, &obs, &full) != 4)
-        return fprintf(stdout, "map error\n"), 1;
-    if (empty == obs || empty == full || full == obs)
-        return fprintf(stdout, "map error\n"), 1;
+    if (fscanf(fd, "%d%c%c%c\n", &height, &empty, &obs, &full) != 4)
+        return (fprintf(stderr, "map error\n"), 1);
+    if (empty == obs || empty == full || obs == full)
+        return (fprintf(stderr, "map error\n"), 1);
     char **map = calloc(height, sizeof(char *));
     if (!map)
-        return fprintf(stdout, "malloc error\n"), 1;
+        return (fprintf(stderr, "malloc error\n"), 1);    
     for (int i = 0; i < height; i++) {
         char *line = NULL;
         size_t len = 0;
-        if (getline(&line, &len, ff) < 0)
-            return fprintf(stdout, "map error\n"), 1;
-        int l = ft_strlen(line);
+        if (getline(&line, &len, fd) < 0)
+            return (fprintf(stderr, "map error\n"), 1);
+        int l = strlenn(line);
         if (l > 0 && line[l - 1] == '\n')
-            line[--l] = '\0';
+            line[l--] = '\0';
         if (!width)
             width = l;
         else if (l != width)
-            return fprintf(stdout, "map error\n"), 1;
+            return (fprintf(stderr, "map error\n"), 1);
         map[i] = line;
     }
-    if (ff != stdin)
-        fclose(ff);
+    if (fd != stdin)
+        fclose(fd);
     int **data = calloc(height + 1, sizeof(int *));
     for (int i = 0; i <= height; i++) {
         data[i] = calloc(width + 1, sizeof(int));
     }
-    square sq = {0, 0, 0};
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
+    square sq = {0,0,0};
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
             if (map[i][j] == empty) {
                 data[i + 1][j + 1] = 1 + min3(data[i][j + 1], data[i + 1][j], data[i][j]);
                 if (sq.size < data[i + 1][j + 1])
                     sq = (square){i, j, data[i + 1][j + 1]};
             }
-            else if (map[i][j] != obs) {
-                return fprintf(stdout, "map error\n"), 1;
-            }
+            else if (map[i][j] != obs)
+                return (fprintf(stderr, "map error\n"), 1);
         }
     }
-    for (int i = sq.x - sq.size + 1; i <= sq.x; i++)
-    {
-        for (int j = sq.y - sq.size + 1; j <= sq.y; j++)
+    for (int i = sq.x - sq.size + 1; i < sq.x; i++) {
+        for (int j = sq.y - sq.size + 1; i < sq.x; j++) {
             map[i][j] = full;
+        }
     }
     for (int i = 0; i < height; i++) {
-        fprintf(stdout, "%s\n", map[i]);
+        fprintf(stdout, "%s", map[i]);
         free(map[i]);
     }
     free(map);
-    for (int i = 0; i <= height; i++)
+    for (int i = 0; i <= height; i++) {
         free(data[i]);
+    }
     free(data);
     return 0;
 }
--- exam05/mylvl2/game_of_life/game_of_life.c
+++ exam05/mylvl2/game_of_life/game_of_life.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(int ac, char **av) {
-    if (ac != 4) {
+    if (ac != 4)
         return 1;
-    }
     int w = atoi(av[1]);
     int h = atoi(av[2]);
     int it = atoi(av[3]);
     int arr[2][w + 2][h + 2];
-    for (int i = 0; i < 2; i++)
-    {
-        for (int j = 0; j < h + 2; j++)
-        {
-            for (int k = 0; k < w + 2; k++)
+    for (int i = 0; i < 2; i++){
+        for (int j = 0; j < h + 2; j++) {
+            for (int k = 0; k < w + 2; k++) {
                 arr[i][k][j] = 0;
+            }
         }
     }
     int x = 1, y = 1, p = 0;
@@ -34,15 +32,11 @@ int main(int ac, char **av) {
         if (p)
             arr[0][x][y] = 1;
     }
-    for (int i = 0; i < it; i++)
-    {
-        for (int j = 1; j < h + 1; j++)
-        {
-            for (int k = 1; k < w + 1; k++)
-            {
+    for (int i = 0; i < it; i++) {
+        for (int j = 1; j < h + 1; j++) {
+            for (int k = 1; k < w + 1; k++) {
                 int nb = 0;
-                for (int y = -1; y <= 1; y++)
-                {
+                for (int y = -1; y <= 1; y++) {
                     for (int x = -1; x <= 1; x++) {
                         if (!(x == 0 && y == 0))
                             nb += arr[i % 2][k + x][j + y];
@@ -63,11 +57,9 @@ int main(int ac, char **av) {
             }
         }
     }
-    for (int i = 1; i < h + 1; i++)
-    {
-        for (int j = 1; j < w + 1; j++)
-        {
-            putchar(arr[it % 2][j][i] ? '0' : ' ');
+    for (int i = 0; i < h + 1; i++) {
+        for (int j = 0; j < h + 1; j++) {
+            putchar(arr[it % 2][i][j] ? '0': ' '); // can be O not 0 read ur subject
         }
         putchar('\n');
     }
